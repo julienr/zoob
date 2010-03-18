2b0a0274cdff08e43871569953c73d2d17aaa347
Julien Rebetez <julien@fhtagn.net>
2010-03-18 14:41:37 +0100

    Removed libpng and libzip from project, build as separate "app" and statically link to project

--- project/jni/Android.mk
+++ project/jni/Android.mk
@@ -1,2 +1,33 @@
 LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := moob
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libzip/ $(LOCAL_PATH)/../libpng/
+#LOCAL_STATIC_LIBRARIES := libzip libpng
+
+#-Wno-psabi to remove warning about GCC 4.4 va_list warning
+LOCAL_CFLAGS := -ggdb -DANDROID_NDK -Wno-psabi
+
+LOCAL_DEFAULT_CPP_EXTENSION := cpp 
+
+LOCAL_SRC_FILES := \
+    app-android.cpp \
+    lib/Vector2.cpp \
+    logic/BoundingBox.cpp \
+    logic/Viewable.cpp \
+    logic/Level.cpp \
+    logic/Game.cpp \
+    levels/LevelsData.cpp \
+    view/TextureManager.cpp \
+    view/Sprite.cpp \
+    view/Square.cpp \
+    view/LevelView.cpp \
+    view/GameView.cpp
+
+LIBPNG_PATH := /home/silver/programs/android-ndk-r3/out/apps/libpng/armeabi/
+LIBZIP_PATH := /home/silver/programs/android-ndk-r3/out/apps/libzip/armeabi
+LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog -lz -L$(LIBPNG_PATH) -lpng -L$(LIBZIP_PATH) -lzip
+
+include $(BUILD_SHARED_LIBRARY)
