4530a97af9cb33a4b0e011a0dcfdf9d22689d35b
Julien Rebetez <julien@fhtagn.net>
2010-09-24 15:10:35 +0200

    pathfinding now uses pathfinder

--- project/jni/ai/algorithms/PathFinder.cpp
+++ project/jni/ai/algorithms/PathFinder.cpp
@@ -1,11 +1,11 @@
 #include "PathFinder.h"
 
-PathFinder::PathFinder (const Grid& grid) : astar(grid, &reservationTable) {
+PathFinder::PathFinder (const Grid& grid) : astar(grid) {
 
 }
 
-Path* PathFinder::findPath (const Vector2& start, const Vector2& end, Entity* e, float entityMoveSpeed, StopCondition* cond) {
-  return astar.shortestWay(start, end, e, entityMoveSpeed, cond);
+Path* PathFinder::findPath (const Vector2& start, const Vector2& end, Entity* e, StopCondition* cond) {
+  return astar.shortestWay(start, end, e, cond);
 }
 
 bool PathFinder::needsReplanning (Entity* e) {
